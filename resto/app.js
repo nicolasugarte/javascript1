648e35a84d62b95b4008e4d38891038de37fa432
Nicolas Ugarte <nicolas.ugarte@live.com.ar>
2022-03-16 15:11:52 -0300

    prueba

---
+++ resto/app.js
@@ -0,0 +1,30 @@
+let nombre = prompt("Ingresa tu primer nombre y apellido todo en minusculas");
+alert("Hola " + nombre + " bienvenido")
+alert("si me permites, te dire como te llaman")
+
+
+if (nombre == "marcos marello") {
+    alert("TE DICEN GORDO MUFA")
+} else if (nombre == "martin strizic") {
+    alert("TE DICEN RUSO PUTO")
+} else if (nombre == "gaston imamura") {
+    alert("TE DICEN LOBO HIJO DE PUTA")
+} else if (nombre == "ezequiel lopez pedraza") {
+    alert("TE DICEN CONEJO, DOCTOR Y CONTADOR")
+} else if (nombre == "lucas strizic") {
+    alert("TE DICEN RUSITO JR COLORINCHE")
+} else if (nombre == "javier lopez") {
+    alert("TE DICEN MANICHE")
+} else if (nombre == "fernando kocsis") {
+    alert("JAJAJA TE LESIONASTE HACIENDO CLICK")
+} else if (nombre == "facundo mamone") {
+    alert("TE DICEN TANO Y PELADO")
+} else if (nombre == "martin galmes") {
+    alert("mg3, retirado para siempre")
+} else if (nombre == "ramiro alfano") {
+    alert("granada, protestito y calefon")
+} else if (nombre == "facundo mamone") {
+    alert("TE DICEN TANO Y PELADO")
+} else if (nombre = "martin duraes") {
+    alert(" TE DICEN NOCHA PUTO")
+}
